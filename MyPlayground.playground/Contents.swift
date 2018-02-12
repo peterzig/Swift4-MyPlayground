eb04ac2ccfef5304a13cebf5c307f243a8e49a15
Peter Zaporowski <peter@Peters-Mac.local>
2018-02-12 15:05:22 +0100

    third commit

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -171,8 +171,8 @@ for (index,value) in arraySix.enumerated() {
 }
 
 
-var city = ["Boston", "London", "Chicago", "Atlanta"]
-let filtr = city.filter{$0.range(of:"o") != nil}
+var cities = ["Boston", "London", "Chicago", "Atlanta"]
+let filtr = cities.filter{$0.range(of:"o") != nil}
 
 //Dictionaries
 
@@ -215,5 +215,225 @@ var newSetExclusiveOr = mySet1.symmetricDifference(mySet2)
 mySet1.removeAll()
 
 //Tuples
+
+var team = ("Boston","Red Sox",97,65,59.9)
+//var (city, namez, wins,loses, percent) = team
+
+ var city = team.0
+ var namez = team.1
+ var wins = team.2
+ var loses = team.3
+ var percent = team.4
+
+var team1 = (city: "Boston", namez: "Red Sox", wins: 97, loses: 65, percent: 59.9)
+
+let teamOneScore = 7
+let teamTwoScore = 6
+if teamOneScore > teamTwoScore {
+    print("Team one won")
+} else if teamTwoScore > teamOneScore{
+    print("Team two won")
+} else {
+    print("It's a tie")
+}
+
+var ex = 9
+if ex > 10 {
+    // code here
+} else {
+    // error code
+}
+
+func guardFunction(str: String?) {
+    guard let goodStr = str else {
+        print("input was nil")
+        return
+    }
+    print("input was \(goodStr)")
+}
+
+for index in 1...10 {
+    print(index)
+}
+
+var dic = ["PL": "Poland", "UK": "United Kingdom", "US": "United States"]
+
+for (abbr, name) in dic {
+    print("\(abbr) -- \(name)")
+}
+
+var ran = 0
+while ran < 7 {
+    ran = Int(arc4random_uniform(10))
+}
+
+var run: Int
+repeat {
+    run = Int(arc4random_uniform(10))
+} while run < 4
+
+var num = 5
+switch num {
+case 1:
+    print("number is one")
+case 2:
+    print("number is two")
+case 3:
+    print("number is three")
+default:
+    print("number is \(num)")
+}
+
+var char : Character = "e"
+switch char {
+case "a", "e", "i", "o", "u":
+    print("letter is a vowel")
+case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m","n", "p", "q", "r",
+     "s", "t", "v", "w","x", "y", "z":
+    print("letter is a consonant")
+default:
+    print("unknown letter")
+}
+
+var grade = 93
+var studentID = 1
+switch grade {
+case 90...100 where studentID == 1:
+    print("Grade is an A for student 1")
+case 80...89:
+    print("Grade is a B")
+case 70...79:
+    print("Grade is an C")
+case 60...69:
+    print("Grade is a D")
+case 0...59:
+    print("Grade is a F")
+default:
+    print("Unknown Grade")
+}
+
+enum Product {
+    case Book(String, Double, Int)
+    case Puzzle(String, Double)
+}
+var order = Product.Book("Mastering Swift 4", 49.99, 2017)
+switch order {
+case .Book(let name, let price, let year):
+    print("You ordered the book \(name): \(year) for \(price)")
+case .Puzzle(let name, let price):
+    print("You ordered the puzzle \(name) for \(price)")
+}
+
+for numbah in 1...100 {
+    if numbah % 2 == 0 {
+        print(numbah)
+    }
+}
+
+for number1 in 1...30 where number1 % 2 == 0 {
+    print(number1)
+}
+
+var worldSeriesWinners = [
+    ("Red Sox", 2004),
+    ("White Sox", 2005),
+    ("Cardinals", 2006),
+    ("Red Sox", 2007),
+    ("Phillies", 2008),
+    ("Yankees", 2009),
+    ("Giants", 2010),
+    ("Cardinals", 2011),
+    ("Giants", 2012),
+    ("Red Sox", 2013),
+    ("Giants", 2014),
+    ("Royals", 2015)
+]
+for case let ("Red Sox", year) in worldSeriesWinners {
+    print(year)
+}
+
+let myNumbers: [Int?] = [1,2,nil,4,5,nil,6]
+
+for case let .some(num) in myNumbers {
+    print(num)
+}
+
+for case let num? in myNumbers where num > 3 {
+    print(num)
+}
+
+for num in myNumbers {
+    if let num = num {
+        if num > 3 {
+            print(num)
+        }
+    }
+}
+
+enum Identifier {
+    case Name(String)
+    case Number(Int)
+    case NoIdentifier
+}
+var playerIdentifier = Identifier.Number(2)
+if case let .Number(num) = playerIdentifier {
+    print("Player's number is \(num)")
+}
+
+for i in 1...10 {
+    if i % 2 == 0 {
+        continue
+    }
+    print("\(i) is odd")
+}
+
+for i in 1...10 {
+    if i % 2 == 0 {
+        break
+    }
+    print("\(i) is odd")
+}
+
+// Swift switch doesn't fall through, we have to force it
+var nameth = "Jon"
+var sport = "Baseball"
+switch sport {
+    case "Baseball":
+        print("\(nameth) plays Baseball")
+        fallthrough
+    case "Basketball":
+        print("\(nameth) plays Basketball")
+        fallthrough
+    default:
+        print("Unknown sport")
+}
+
+//Functions
+func sayHello(name: String) -> Void {
+    let retString = "Hello " + name
+    print(retString)
+}
+
+sayHello(name: "Peter")
+
+func sayHello2(name: String) -> String {
+    let retString = "Hello " + name
+    return retString
+}
+
+sayHello2(name: "Peter")
+
+func sayHello3(name: String, greeting: String) {
+    print("\(greeting) \(name)")
+}
+
+sayHello3(name: "Tavarish", greeting: "Zdrastvujtie")
+
+func getNames() -> [String] {
+    let retArray = ["Jon", "Peter", "Vadim", "Dimitry"]
+    return retArray
+}
+
+var namezzzz = getNames()
 
 
